294116155978810cfd18fecde0d75f1b572014d0
Lucas chagas <lucazchagaz@outlook.com>
2023-06-06 23:35:01 -0300

    add isLoading props for show activity indicador

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import {ActivityIndicator, TouchableOpacityProps} from 'react-native';
 import {Container} from './styles';
 
-export type IButtonProps = {
+export interface IButtonProps extends TouchableOpacityProps {
   width?: number;
   height?: number;
   bg?: string;
+  isLoading?: boolean;
   children: React.ReactNode;
-};
+}
 
-export default function Button({width, height, bg, children}: IButtonProps) {
+export default function Button({
+  width,
+  height,
+  bg,
+  children,
+  isLoading,
+  ...rest
+}: IButtonProps) {
   return (
-    <Container width={width} height={height} bg={bg}>
-      {children}
+    <Container width={width} height={height} bg={bg} {...rest}>
+      {isLoading ? <ActivityIndicator size={25} color="#FFF" /> : children}
     </Container>
   );
 }
