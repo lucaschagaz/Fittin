4456136fb768795c2ba17ee3b26d365f0e672a7f
Lucas chagas <lucazchagaz@outlook.com>
2023-06-19 20:48:46 -0300

    add choose sex page

---
+++ src/Pages/ChooseSex/index.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {Button, Content, Header, Title} from '../../Components';
+import {ButtonContainer, ButtonImage, Container, Image} from './styles';
+import {View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+export default function ChooseSex() {
+  const [sex, setSex] = useState('');
+
+  const navigation = useNavigation();
+
+  return (
+    <Content>
+      <Header>
+        <Title size={24} weight="bold">
+          Preciso de algumas informações!
+        </Title>
+        <Title weight="bold" color="GRAY">
+          informações necessarias para criação do seu plano personalizado
+        </Title>
+      </Header>
+      <Container>
+        <ButtonContainer>
+          <View>
+            <ButtonImage
+              style={{borderColor: sex == 'men' ? '#3447d4' : '#9999'}}
+              onPress={() => setSex('men')}>
+              <Image source={require('../../Assets/man.png')} />
+            </ButtonImage>
+            <Title>Homen</Title>
+          </View>
+          <View>
+            <ButtonImage
+              style={{borderColor: sex == 'woman' ? '#3447d4' : '#9999'}}
+              onPress={() => setSex('woman')}>
+              <Image source={require('../../Assets/woman.png')} />
+            </ButtonImage>
+            <Title>Mulher</Title>
+          </View>
+        </ButtonContainer>
+      </Container>
+      <Button
+        width={50}
+        style={{alignSelf: 'flex-end'}}
+        onPress={() => navigation.navigate('Measurements')}>
+        <Title weight="bold" color="TEXT">
+          Proximo passo
+        </Title>
+      </Button>
+    </Content>
+  );
+}
---
+++ src/Pages/ChooseSex/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+export const ButtonContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const Image = styled.Image`
+  width: 125px;
+  height: 125px;
+  border-radius: 125px;
+`;
+
+export const ButtonImage = styled.TouchableOpacity`
+  margin: 0px 10px;
+  width: 125px;
+  height: 125px;
+  border-radius: 125px;
+  background-color: ${({theme}) => theme.COLORS.GRAY_100};
+  border-width: 4px;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 15px;
+`;
