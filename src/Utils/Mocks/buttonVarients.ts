1d166fab0577ae4489b0fed4dbe702840d6c2da0
Lucas chagas <lucazchagaz@outlook.com>
2023-09-06 07:41:49 -0300

    Feat: added mock for buttons veriants will be used

---
+++ src/Utils/Mocks/buttonVarients.ts
@@ -0,0 +1,5 @@
+const variants = [
+  {prymary: {btnBg: '#3447d4', btnTextColor: '', btnBorderColor: ''}},
+  {PrimaryDisable: {btnBg: '#3447d4', btnTextColor: '', btnBorderColor: ''}},
+  {secondary: {btnBg: '#3447d4', btnTextColor: '', btnBorderColor: ''}},
+];
