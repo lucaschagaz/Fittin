9386e2dce079e7b5819ccc2e184e9bec9a48c153
Lucas chagas <lucazchagaz@outlook.com>
2023-09-07 21:08:40 -0300

    Feat: added buttons variants colors and props

--- src/Utils/Mocks/buttonVarients.ts
+++ src/Utils/Mocks/buttonVarients.ts
@@ -1,5 +1,22 @@
-const variants = [
-  {prymary: {btnBg: '#3447d4', btnTextColor: '', btnBorderColor: ''}},
-  {PrimaryDisable: {btnBg: '#3447d4', btnTextColor: '', btnBorderColor: ''}},
-  {secondary: {btnBg: '#3447d4', btnTextColor: '', btnBorderColor: ''}},
-];
+export const variants = {
+  primary: {
+    backgroundColor: 'PRIMARY',
+    color: 'PRIMARY_CONTRAST',
+    BorderColor: 'PRIMARY',
+    BorderWidht: 0,
+  },
+  PrimaryLight: {
+    backgroundColor: 'GRAY_Light',
+    color: 'GRAYDARK',
+    BorderColor: 'GRAY',
+    BorderWidht: 1,
+  },
+  secondary: {
+    backgroundColor: 'transparent',
+    color: 'GRAY',
+    BorderColor: 'GRAY',
+    BorderWidht: 1,
+  },
+};
+
+export type variantType = (typeof variants)[keyof typeof variants];
