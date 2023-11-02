b0cfc86a043ca0fac59400cf2126c7a0291e554e
Lucas chagas <lucazchagaz@outlook.com>
2023-11-02 11:31:05 -0300

    refector: remove View in High level with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import Routes from './Routes/routes';
+import {Routes} from './Routes/routes';
 import {ThemeProvider} from 'styled-components';
 
 import {Theme} from './Styles/Theme/ligth';
 
-export default function App() {
+export const App = () => {
   return (
     <ThemeProvider theme={Theme}>
-      <View style={styles.Conteiner}>
-        <Routes />
-      </View>
+      <Routes />
     </ThemeProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  Conteiner: {
-    flex: 1,
-    width: '100%',
-  },
-});
+};
